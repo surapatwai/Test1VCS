6dd92798e9f1d115d9178962b130199ef404a719
SurapatWai <wa.surapat_st@tni.ac.th>
2017-02-03 01:28:47 +0000

    Commit1 - First version

---
+++ src/generate_list.py
@@ -0,0 +1,21 @@
+import random
+def generate_list():
+    alist = [x for x in range(random.randint(-10,10))]
+    return alist
+
+"""
+print a generate_list
+"""
+def printIt()
+    print(generate_list())
+
+def main():
+    printIt()
+
+"""
+IF this script file is called, it will run main() directly
+"""
+
+if __name__ == '__main__' :
+    print("Test printIT():")
+    main()
